32d8f66949277480fd3c61f867e53ab96e5371c5
Nate <theredpea@gmail.com>
2020-01-08 18:41:32 -0700

    circle size

---
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -0,0 +1,80 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// let tile_url = 'https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/{z}/{x}/{y}.png?access_token={accessToken}';
+let tile_url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
+// let tile_url = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}';
+// let tile_url = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+
+let streets = L.tileLayer(tile_url, {
+    attribution: 'Map data %copy;  <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // maxZoom: 5,
+
+    // Different styles:
+    // id: 'mapbox.streets',
+    // id: 'mapbox.wheatpaste',
+    // id: 'mapbox.comic',
+    // id: 'mapbox.pirates',
+    // id: 'satellite-streets-v11',
+    accessToken: API_KEY
+});
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid', {
+    center: [44.0, -80.0],
+    zoom: 5,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+// streets.addTo(map);
+
+let earthquake7DayUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+
+// Grabbing our GeoJSON data.
+d3.json(earthquake7DayUrl).then(function (data) {
+    console.log(data);
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: "#ffae42",
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 4;
+    }
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo
+    }).addTo(map);
+
+});
