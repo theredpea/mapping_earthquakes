99f033f1c17c5e130dbcf2845cd7fb1171e66241
Nate <theredpea@gmail.com>
2020-01-08 18:44:25 -0700

    Color and popup

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -49,11 +49,31 @@ let earthquake7DayUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summa
 // Grabbing our GeoJSON data.
 d3.json(earthquake7DayUrl).then(function (data) {
     console.log(data);
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+            return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+            return "#ea822c";
+        }
+        if (magnitude > 3) {
+            return "#ee9c00";
+        }
+        if (magnitude > 2) {
+            return "#eecc00";
+        }
+        if (magnitude > 1) {
+            return "#d4ee00";
+        }
+        return "#98ee00";
+    }
+
     function styleInfo(feature) {
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: "#ffae42",
+            fillColor: getColor(feature.properties.mag),
             color: "#000000",
             radius: getRadius(feature.properties.mag),
             stroke: true,
@@ -74,7 +94,10 @@ d3.json(earthquake7DayUrl).then(function (data) {
             return L.circleMarker(latlng);
         },
         // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
+        style: styleInfo,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`Magnitude: ${feature.properties.mag} <br>Location: ${feature.properties.location}`)
+        }
     }).addTo(map);
 
 });
